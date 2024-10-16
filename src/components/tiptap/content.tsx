0dccb9a82df627c2180e771e185a78af4a0cf49b
Braeden Hall <braeden@brhall.dev>
2024-10-16 14:02:29 -0400

    md styling for imgs links and codeblocks

--- src/components/tiptap/content.tsx
+++ src/components/tiptap/content.tsx
@@ -3,8 +3,11 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Markdown } from "tiptap-markdown";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import Image from "@tiptap/extension-image";
 import Typography from "@tiptap/extension-typography";
-import Link from "@tiptap/extension-link";
+import  Link  from "./custom-link";
+import { all, createLowlight } from "lowlight";
 
 import "@/components/tiptap/styles.css";
 import { forwardRef, useImperativeHandle } from "react";
@@ -18,18 +21,31 @@ export interface TiptapContentRef {
   getEditor: () => ReturnType<typeof useEditor> | null;
 }
 
+const lowlight = createLowlight(all);
+
+
 const TiptapContent = forwardRef<TiptapContentRef, TiptapProps>(({ content, editable }, ref) => {
   const editor = useEditor({
     extensions: [
+      Image,
+      Typography,
+      Link.configure({
+        autolink: false,
+      }),
+      CodeBlockLowlight.configure({
+        lowlight,
+        HTMLAttributes: {
+          class: 'code-block',
+        },
+      }),
       Markdown.configure({
         tightLists: false,
       }),
-      Typography,
-      Link,
       StarterKit.configure({
         heading: {
           levels: [1, 2, 3],
         },
+        codeBlock: false,
       }),
     ],
     content: content,
